0a9e7fc5752dc6a489300947cc9ae9abccbc6c39
marciopaulosoares <marcio@marciopaulo.com.br>
2023-04-08 12:07:22 -0300

    test chaining spans

--- jaeger-spring/src/main/java/br/com/marciopaulo/jaegerspring/HelloController.java
+++ jaeger-spring/src/main/java/br/com/marciopaulo/jaegerspring/HelloController.java
@@ -1,21 +1,37 @@
 package br.com.marciopaulo.jaegerspring;
+
 import io.jaegertracing.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public  RestTemplate restTemplate(){
+        return  new RestTemplate();
+    }
+    @Bean
+    public io.opentracing.Tracer initTracer(){
+        Configuration.SamplerConfiguration samplerConfiguration = new Configuration.SamplerConfiguration().withType("const").withParam(1);
+        Configuration.ReporterConfiguration reporterConfiguration = Configuration.ReporterConfiguration.fromEnv().withLogSpans(true);
+        return Configuration.fromEnv("boot-service").withSampler(samplerConfiguration).withReporter(reporterConfiguration).getTracer();
+    }
 
     @RequestMapping("/hello")
     public String hello(){
         return  "Hello from spring boot";
     }
 
-    @Bean
-    public io.opentracing.Tracer initTracer(){
-        Configuration.SamplerConfiguration samplerConfiguration = new Configuration.SamplerConfiguration().withType("const").withParam(1);
-        Configuration.ReporterConfiguration reporterConfiguration = Configuration.ReporterConfiguration.fromEnv().withLogSpans(true);
-        return Configuration.fromEnv("boot-service").withSampler(samplerConfiguration).withReporter(reporterConfiguration).getTracer();
+    @RequestMapping("/chaining")
+    public String chaining(){
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/hello", String.class);
+        return "Chaining response"+response.getBody();
     }
 }
